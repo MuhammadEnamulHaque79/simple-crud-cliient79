cb98e11428cc2797631d3e1e09ef6ff8e13e7300
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-05-13 22:00:10 +0600

    yes i can

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,40 +3,37 @@ import React from 'react';
 import './Users.css';
 import { useLoaderData } from 'react-router-dom';
 
-
 const Users = () => {
     const users = useLoaderData();
-    const handleDeleteUser =(_id)=>{
+    // console.log(users);
+
+    const handleUserDelete =(_id)=>{
         console.log('DELETE',_id);
         fetch(`http://localhost:5000/users/${_id}`,{
-            method: 'DELETE',
+            method:'DELETE',
         })
             .then(res => res.json())
             .then(data =>{
-                console.log(data)
-                if (data.deletedCount === 1) {
-                    console.log("Successfully deleted one document.");
-                  } else {
-                    console.log("No documents matched the query. Deleted 0 documents.");
-                  }
+                console.log(data);
             })
     }
-   
+    
     return (
         <div>
-            <h2>Hello users!! : {users.length}</h2>
-            <div className='user-info-container'>
-                {
-                    users.map(user =><p className='user-info' key={user._id}>
-                            {user.name}:{user._id}:<button className='btn'
-                            onClick={()=>handleDeleteUser(user._id)}
-                            >Delete</button>
-                    </p>)
-                }
+            <h1>Users : {users.length} </h1>
+            <div  className='user-info'>
+                    {
+                        users.map(user =><p className='user' key={user._id}>
+                            {user.name}:{user.email}:{user._id}
+                            <button onClick={()=>handleUserDelete(user._id)} className='btn'>Delete</button>
+                        </p>)
+                    }
             </div>
         </div>
     );
 };
 
 export default Users;
-                            
+
+//67_7 start
+
